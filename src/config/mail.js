9c2419c9c39c2053aabb9375740f5037b0e7738d
Daniel Santiago <danielsantiago111@hotmail.com>
2020-10-07 19:23:50 -0300

    Email config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '431ec90442400b',
+    pass: '4db781ffaaebc2',
+  },
+  default: {
+    from: 'Barbershop Team <noreply@barbershop.com',
+  },
+};
